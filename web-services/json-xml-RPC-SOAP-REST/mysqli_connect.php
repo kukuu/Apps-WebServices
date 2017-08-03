785b69707288bff354b0bf861f8581690d2884dd
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2017-08-03 08:46:50 +0100

    Update mysqli_connect.php

--- web-services/json-xml-RPC-SOAP-REST/mysqli_connect.php
+++ web-services/json-xml-RPC-SOAP-REST/mysqli_connect.php
@@ -1,12 +1,13 @@
 
 <?php
 //Author: Alexander Adu-Sarkodie
-// Application Type : Web Service:  
+// Application Type : Web Service: 
+//Best practice to use variables/constants to hold configuration dependencies. Allows easy maintenance and scalability
 
 	DEFINE ('DB_USER' ,'studentweb');
-	DEFINE ('DB_PASSWORD' ,'mywizzardbabes');
+	DEFINE ('DB_PASSWORD' ,'your-password');
 	DEFINE ('DB_HOST' ,'localhost');
-	DEFINE ('DB_NAME' ,'testdb');
+	DEFINE ('DB_NAME' ,'your-db');
 
 //Make connection or manage error handling
 	$dbc = @mysqli_connect(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME) 
