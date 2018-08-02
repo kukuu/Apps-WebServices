260e75efd6dc959a31c34f402940c5390c3bb53a
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2018-08-02 11:50:41 +0100

    More scopes to XMLHttp ready states.

--- polyglonomy/generate-JSON-AJAX-datasets/JavaScript/app.js
+++ polyglonomy/generate-JSON-AJAX-datasets/JavaScript/app.js
@@ -1,9 +1,17 @@
 //app.js
 
 var results = document.getElementById('results');
+// Create an instance of the XMLHttp object
 var r = new XMLHttpRequest();
+// Value	State	Description
+//0	UNSENT	Client has been created. open() not called yet.
+//1	OPENED	open() has been called.
+//2	HEADERS_RECEIVED	send() has been called, and headers and status are available.
+//3	LOADING	Downloading; responseText holds partial data.
+//4	DONE	The operation is complete.
 r.open("GET", "http://www.filltext.com?rows=10&f={firstName}", true);
 r.onreadystatechange = function () {
+  //XMLHttp ready states
   if (r.readyState != 4 || r.status != 200) return;
   var data = JSON.parse(r.responseText);
   for (i=0;i<data.length;i++){
