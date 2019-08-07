4b7d20d680e6a0a77850fca15285d9455cb0a388
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-08-07 17:38:17 +0100

    Updated client.js

--- php/scripts/client.js
+++ php/scripts/client.js
@@ -1,5 +1,7 @@
+	
+//Author: Alexander Adu-Sarkodie
 
-function getAllStudents()
+function GetAllStudents()
 {
 	let formRequest = newFormData();
 	// append to header
@@ -7,7 +9,27 @@ function getAllStudents()
 
 	//Make an AJAX request
 	let xhr = new XMLHttpRequest();
-	xhr.addEventListener('load', uplodComplete, false);
+	xhr.addEventListener('load', uploadComplete, false);
 	xhr.open('GET', '/StudentService/getAllStudents.php')
 	xhr.send(formRequest);
+}
+
+//Make request for specific IDS
+
+function GetStudentByID(id)
+{
+	let formRequest = new FormData();
+
+	//attaching key/value to header for recognition
+	newFormData.append('sid', id)
+
+	//making AJAX request to DB
+	let xhr = new XMLHttpRequest();
+	xhr.addEventListener('load', uploadComplete, false);
+	xhr.open('POST', 'StudentService/GetStudentByID.php');
+	xhr.send(formRequest);
+}
+
+function uploadComplete(evt){
+	console.log(evt.target.responeText);
 }
