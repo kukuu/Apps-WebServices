87e215a58a9658f9aa40f255949f41324e898db6
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-08-07 17:36:57 +0100

    Adding getAllStudents.php service
    1. Connection Manager
    2. Callbacks
    3. Response algorithm
    3. Embedded View Template

---
+++ php/StudentService/getAllStudents.php
@@ -0,0 +1,43 @@
+
+<?php
+
+	//Author: Alexander Adu-Sarkodie
+	
+	require_once '../ConnectionManager.php';
+
+	$response =  array();
+
+	//Connecting to MySQL database
+	//Singleton function
+	$db = ConnectionManager::getInstance();
+
+	$result = mysql_query("Select from Student") or die(mysql_error());
+
+	if(mysql_num_rows($result) > 0)
+	{
+		$response['student'] = array();
+
+		while($row = mysql_fetch_array($result))
+		{
+			$student = array();
+			$student["ID"] = $row["ID"];
+			$student["Index"] = $row["Index"];
+			$student["Name"] = $row["Name"];
+
+			array_push($response["student"], $student)	
+		}
+
+		$response["success"] = 1;
+
+		//send back JSON response
+		echo json_encode(($response));
+	}
+
+	else
+	{
+		$response["success"] = 0;
+		$response["message"] = "No student found!";
+
+		echo json_encode($response);
+	}
+?>
