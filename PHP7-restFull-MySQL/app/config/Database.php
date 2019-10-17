3c1f026e088e4cc4c88de1a08a8e87f09ba3d063
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-10-17 11:59:40 +0100

    Updated Database.php with context.

--- PHP7-restFull-MySQL/app/config/Database.php
+++ PHP7-restFull-MySQL/app/config/Database.php
@@ -24,10 +24,10 @@
 			$this->conn = null;
 
 			try {
-				//Instantiate  a PDO
+				//Instantiate  a PDO connection object
 				$this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
 
-				//Set error mode
+				//Set error mode, and trigger exception
 				$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 			} catch(PDOException $e) {
