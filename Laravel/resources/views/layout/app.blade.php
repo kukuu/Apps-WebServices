e90fbca49ad8bb848ee390cff5890c0fc1fa215f
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-10-21 13:51:52 +0100

    Updated FORMS with CK Editor formatting module.

--- Laravel/resources/views/layout/app.blade.php
+++ Laravel/resources/views/layout/app.blade.php
@@ -14,6 +14,10 @@
 			@include('inc.messages')
 			@yield('content')
 		</div>
+		<script src="/vendor/unisharp/laravel-ckeditor/checkdir.js"></script>
+		<script>
+			CKEDITOR.replace('article-ckeditor');
+		</script>
 	</body>
 
 </html>
