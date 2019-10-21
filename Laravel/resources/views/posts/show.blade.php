6a35feddd5ff3cb546140fa422f56316c91dd8f7
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-10-21 14:01:32 +0100

    Updated show template to parse html.

--- Laravel/resources/views/posts/show.blade.php
+++ Laravel/resources/views/posts/show.blade.php
@@ -5,7 +5,7 @@
 	<a href="/posts" class="btn btn-default"> Go Back </a>
 	<h1> {{$posts->title}}</h1>
 	<div>
-			{{$posts->body}}
+			{!!$posts->body!!}
 	</div>
 	<hr />
 	<small>Written on {{$post->created_at }</small>
