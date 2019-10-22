bac44761c7c3832cb3930e455c691edd445f8b6c
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-10-22 14:51:32 +0100

    PostController's index and create methods for app.

---
+++ Laravel/app/http/controllers/PostController.php
@@ -0,0 +1,38 @@
+
+class PostController extends Controller
+{
+	/**
+	* Display a listing of the resource.
+	* @return @illuminate\Http\Response
+	*
+	*/
+
+	public function index()
+	{
+
+		//posts = Post::all();
+		//return Post::where('title', 'Post Two')->get();
+		//$posts = DB::select('SELECT * FROM posts');
+		//$posts = Post::orderBy('title', 'desc')->take(1)->get();
+		//$posts = Post::orderBy('title', 'desc')->get();
+
+		$posts = Post::orderBy('title', 'desc')->paginate(10);
+		return view('posts.index')->with('posts', $posts);
+	}
+
+
+	/**
+	* Show the form for creating a new resource
+	*
+	* @return \Illuminate\Http\Response
+
+	*/
+
+	public function create()
+	{
+		return view('posts.create');
+	}
+
+
+
+}
